d762c6d9432b3be85b3d99f3911a289b3e191c32
yx8888 <3981895860@qq.com>
2026-02-01 22:23:40 +0800

    1

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ window.onload = function() {
             var replyMessage = document.createElement('div');
             replyMessage.classList.add('chat-message');
             if (message === '请点击第一句获取游戏。') {
-                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「游戏获取」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~a37939qChN~:/链接：https://pan.quark.cn/s/6cc71b4379a7?pwd=scx7</p>';
+                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「游戏获取」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~b89f3AJLjQ~:/链接：https://pan.quark.cn/s/e307f3a9a980</p>';
             } else if (message === '请点击第二句获取安卓和电脑教程。') {
-                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「安卓和电脑教程」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~ffd339qB7A~:/链接：https://pan.quark.cn/s/8ab5938c6c1b?pwd=gNL2</p>';
+                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「安卓和电脑教程」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~88393AJLXo~:/链接：https://pan.quark.cn/s/c1a0777c952d</p>';
             } else if (message === '请点击第三句获取ios教程。') {
-                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「ios教程」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~c0773A5xup~:/链接：https://pan.quark.cn/s/8b708f27c412</p>';
+                replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>我用夸克网盘给你分享了「ios教程」，点击链接或复制整段内容，打开「夸克APP」即可获取。/~aaee3AJLLa~:/链接：https://pan.quark.cn/s/4d0f36a2fbf7</p>';
             } else {
                 replyMessage.innerHTML = '<img src="bot-avatar.jpg" alt="Avatar"/><p>请看前三句话。</p>';
             }
